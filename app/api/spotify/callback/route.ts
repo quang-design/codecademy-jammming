eca51f95c0bc13d46a7c2078934b6f9229e8fd0d
quang-design <anhquang1806@gmail.com>
2025-08-10 14:43:06 +0700

    feat: add error handling for missing code in Spotify callback endpoint

--- app/api/spotify/callback/route.ts
+++ app/api/spotify/callback/route.ts
@@ -1,9 +1,10 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const urlParams = new URLSearchParams(request.url);
-  const code = urlParams.get("code");
-
+export async function GET(request: NextRequest) {
+  const code = request.nextUrl.searchParams.get("code");
+  if (!code) {
+    return NextResponse.json({ error: "Missing code" }, { status: 400 });
+  }
   console.log(code);
 
   return NextResponse.json({ code });
